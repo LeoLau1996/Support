bc0db67aa46d7270b01c3ede2808d6be4445c03b
leo <742831652@qq.com>
2020-08-03 10:00:00 +0800

    fix:代码整理

--- leosupport/src/main/java/leo/work/support/Widget/NGridView.java
+++ leosupport/src/main/java/leo/work/support/Widget/NGridView.java
@@ -26,9 +26,7 @@ public class NGridView extends GridView {
 
     @Override
     public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
-            MeasureSpec.AT_MOST);
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
         super.onMeasure(widthMeasureSpec, expandSpec);
     }
 }
