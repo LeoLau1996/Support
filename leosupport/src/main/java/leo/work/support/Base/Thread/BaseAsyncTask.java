f8ec9a5ff10674e678f0a79037bf4b497b68de74
leo <742831652@qq.com>
2020-08-17 14:09:30 +0800

    feat:BaseAsyncTask注释

--- leosupport/src/main/java/leo/work/support/Base/Thread/BaseAsyncTask.java
+++ leosupport/src/main/java/leo/work/support/Base/Thread/BaseAsyncTask.java
@@ -11,6 +11,12 @@ import android.os.AsyncTask;
  * 代码创建: 刘桂安
  * ---------------------------------------------------------------------------------------------
  * 代码备注:
+ * 缺点
+ * 当线程池已满128时   再新增现场会报RejectedExecutionException
+ * 过多的线程会引起大量消耗系统资源和导致应用FC的风险。
+ * AsyncTask的生命周期与Activity生命周期不同步
+ * 如果在doInBackgroud里有一个不可中断的操作，比如BitmapFactory.decodeStream()，调用了cancle() 也未必能真正地取消任务。
+ * 如果AsyncTask被声明为Activity的非静态的内部类，那么AsyncTask会保留一个对创建了AsyncTask的Activity的引用。如果Activity已经被销毁，AsyncTask的后台线程还在执行，它将继续在内存里保留这个引用，导致Activity无法被回收，引起内存泄露。
  * ---------------------------------------------------------------------------------------------
  **/
 public abstract class BaseAsyncTask extends AsyncTask {
