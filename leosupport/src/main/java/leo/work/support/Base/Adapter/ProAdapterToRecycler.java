37ca700ee311bc00f42c19bc32fa9750ad54b267
leo <742831652@qq.com>
2020-11-30 14:50:00 +0800

    fix：整理代码

--- leosupport/src/main/java/leo/work/support/Base/Adapter/ProAdapterToRecycler.java
+++ leosupport/src/main/java/leo/work/support/Base/Adapter/ProAdapterToRecycler.java
@@ -1,8 +1,6 @@
 package leo.work.support.Base.Adapter;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
