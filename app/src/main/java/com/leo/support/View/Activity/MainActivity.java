d060f15ac24c8e5393bf19e1e904ce9a52f06238
leo <742831652@qq.com>
2021-04-29 17:54:40 +0800

    测试代码

--- app/src/main/java/com/leo/support/View/Activity/MainActivity.java
+++ app/src/main/java/com/leo/support/View/Activity/MainActivity.java
@@ -1,9 +1,45 @@
 package com.leo.support.View.Activity;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
+import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import com.leo.support.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import leo.work.support.base.activity.BaseActivity;
+import leo.work.support.support.common.RecyclerSupport;
+
+public class MainActivity extends BaseActivity {
+
+    private RecyclerView mRecyclerView;
+    private TestAdapter adapter;
+    private List<String> mList;
+
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+        mRecyclerView = findViewById(R.id.mRecyclerView);
+        mList = new ArrayList<>();
+        mList.add("0");
+        mList.add("1");
+        mList.add("2");
+        mList.add("3");
+        adapter = new TestAdapter(context, mList);
+        RecyclerSupport.setLinearLayoutManager(context, mRecyclerView);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+
+    }
 }
---
+++ app/src/main/java/com/leo/support/View/Activity/TestAdapter.java
@@ -0,0 +1,62 @@
+package com.leo.support.View.Activity;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.leo.support.R;
+import com.leo.support.databinding.ItemTestBinding;
+
+import java.util.List;
+
+import leo.work.support.base.adapter.BaseRecyclerMVVMAdapter;
+
+/**
+ * ---------------------------------------------------------------------------------------------
+ * 功能描述:
+ * ---------------------------------------------------------------------------------------------
+ * 时　　间:  2021/4/29
+ * ---------------------------------------------------------------------------------------------
+ * 代码创建: 刘桂安
+ * ---------------------------------------------------------------------------------------------
+ * 代码备注:
+ * ---------------------------------------------------------------------------------------------
+ **/
+public class TestAdapter extends BaseRecyclerMVVMAdapter<String, TestAdapter.MainHolder, ItemTestBinding> {
+
+    public TestAdapter(Context context, List<String> mList) {
+        super(context, mList);
+    }
+
+    @Override
+    protected int setLayout() {
+        return R.layout.item_test;
+    }
+
+    @Override
+    protected MainHolder setViewHolder(ItemTestBinding binding) {
+        return new MainHolder(binding);
+    }
+
+
+    @Override
+    protected void initView(MainHolder holder, int position, String data) {
+        holder.getBinding().setStr(data);
+    }
+
+    @Override
+    protected void initListener(MainHolder holder, int position, String data) {
+
+    }
+
+    static class MainHolder extends BaseRecyclerMVVMAdapter.ViewHolder<ItemTestBinding> {
+
+        public MainHolder(ItemTestBinding binding) {
+            super(binding);
+        }
+
+    }
+}
