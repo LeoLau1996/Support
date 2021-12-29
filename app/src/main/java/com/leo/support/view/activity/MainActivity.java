7df4bad5fbb1e3f6c863e71bac2a1aac01323773
Leo-qlx <742831652@qq.com>
2021-12-29 18:51:57 +0800

    整理代码

--- app/src/main/java/com/leo/support/view/activity/MainActivity.java
+++ app/src/main/java/com/leo/support/view/activity/MainActivity.java
@@ -1,43 +1,7 @@
 package com.leo.support.view.activity;
 
-import androidx.recyclerview.widget.RecyclerView;
+import android.app.Activity;
 
-import android.os.Bundle;
+public class MainActivity extends Activity {
 
-import com.leo.support.R;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import leo.work.support.base.activity.mvc.CommonActivity;
-import leo.work.support.support.common.RecyclerSupport;
-
-public class MainActivity extends CommonActivity {
-
-    private RecyclerView mRecyclerView;
-    private TestAdapter adapter;
-    private List<String> mList;
-
-    @Override
-    protected int setLayout() {
-        return R.layout.activity_main;
-    }
-
-    @Override
-    protected void initData(Bundle savedInstanceState) {
-        mRecyclerView = findViewById(R.id.mRecyclerView);
-        mList = new ArrayList<>();
-        mList.add("0");
-        mList.add("1");
-        mList.add("2");
-        mList.add("3");
-        adapter = new TestAdapter(context, mList);
-        RecyclerSupport.setLinearLayoutManager(context, mRecyclerView);
-        mRecyclerView.setAdapter(adapter);
-    }
-
-    @Override
-    protected void initViews(Bundle savedInstanceState) {
-
-    }
 }
