28ac33d31c1ec16a027dfadb97ae503514026be2
Leo-qlx <742831652@qq.com>
2021-12-29 18:14:00 +0800

    Is修改

--- leosupport/src/main/java/leo/work/support/support/common/Is.java
+++ leosupport/src/main/java/leo/work/support/support/common/Is.java
@@ -33,41 +33,12 @@ import leo.work.support.util.BaseUtil;
  * ---------------------------------------------------------------------------------------------
  **/
 public class Is extends BaseUtil {
-    //验证座机
-    public static boolean isPhone2(String str) {
-        Pattern p1 = null, p2 = null;
-        Matcher m = null;
-        boolean isPhone = false;
-        p1 = Pattern.compile("^[0][1-9]{2,3}-[0-9]{5,10}$");  // 验证带区号的
-        p2 = Pattern.compile("^[1-9]{1}[0-9]{5,8}$");         // 验证没有区号的
-        if (str.length() > 9) {
-            m = p1.matcher(str);
-            isPhone = m.matches();
-        } else {
-            m = p2.matcher(str);
-            isPhone = m.matches();
-        }
-        return isPhone;
-    }
-
-    //验证手机
-    public static boolean isPhone(String phone) {
-        String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
-        if (phone.length() != 11) {
-            return false;
-        } else {
-            Pattern p = Pattern.compile(regex);
-            Matcher m = p.matcher(phone);
-            boolean isMatch = m.matches();
-            return isMatch;
-        }
-    }
 
     public static boolean isEmpty(String text) {
         return TextUtils.isEmpty(text);
     }
 
-    public static boolean isEqualsy(String... texts) {
+    public static boolean isEquals(String... texts) {
         if (texts.length < 2) {
             return false;
         }
@@ -231,23 +202,4 @@ public class Is extends BaseUtil {
         return Pattern.matches(regular, url);
     }
 
-    /**
-     * 判断是否邮箱
-     */
-    public static boolean isEmail(String strObj) {
-        if (isEmpty(strObj)) {
-            return false;
-        }
-        String str = strObj + "";
-        if (!str.endsWith(".com")) {
-            return false;
-        }
-        String match = "\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
-        Pattern pattern = Pattern.compile(match);
-        Matcher matcher = pattern.matcher(str);
-        if (matcher.matches()) {
-            return true;
-        }
-        return false;
-    }
 }
