764bd477d6894858ce51e9b662bb276f39204885
Leo-qlx <742831652@qq.com>
2021-12-31 19:05:34 +0800

    临时提交

--- leosupport/src/main/java/leo/work/support/biz/SmsCountdownBiz.java
+++ leosupport/src/main/java/leo/work/support/biz/SmsCountdownBiz.java
@@ -1,5 +1,13 @@
 package leo.work.support.biz;
 
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import leo.work.support.base.LifeControlInterface;
+import leo.work.support.base.biz.CommonLifeBiz;
+
 /**
  * ---------------------------------------------------------------------------------------------
  * 功能描述: 短信倒计时业务
@@ -11,5 +19,66 @@ package leo.work.support.biz;
  * 代码备注: TODO 设计成单例
  * ---------------------------------------------------------------------------------------------
  **/
-public class SmsCountdownBiz {
+public class SmsCountdownBiz extends CommonLifeBiz {
+
+    //总时长
+    private int totalDuration;
+    //当前时长
+    private int currentDuration;
+    //倒计时
+    private Timer timer;
+    //倒计时回调
+    private OnSmsCountdownBizCallBack callBack;
+
+    public SmsCountdownBiz(LifeControlInterface lifeControlInterface, int totalDuration, OnSmsCountdownBizCallBack callBack) {
+        super(lifeControlInterface);
+        this.totalDuration = totalDuration;
+        this.callBack = callBack;
+    }
+
+    //请求服务器
+    public void requestService() {
+
+    }
+
+    //
+    public void serviceResult(boolean success) {
+        if (!success) {
+            return;
+        }
+        xxx();
+    }
+
+    //开始倒计时
+    private void xxx() {
+        if (timer == null) {
+            timer = new Timer();
+        }
+        timer.cancel();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                int seconds = totalDuration - currentDuration;
+                if (seconds <= 0) {
+                    callBack.end();
+                    timer.cancel();
+                } else {
+                    callBack.onTimeChange(seconds);
+                    currentDuration++;
+                }
+            }
+        }, 1000);
+    }
+
+    public interface OnSmsCountdownBizCallBack {
+
+        //准备
+        void onSmsPrepare();
+
+        //开始倒计时
+        void onTimeChange(int time);
+
+        //
+        void end();
+    }
 }
