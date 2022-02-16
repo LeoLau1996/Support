e317846e8d302b6c3efd8151932cdfcbefefa754
Leo <742831652@qq.com>
2022-02-16 13:58:28 +0800

    修复短信业务bug

--- leosupport/src/main/java/leo/work/support/biz/SmsCountdownBiz.java
+++ leosupport/src/main/java/leo/work/support/biz/SmsCountdownBiz.java
@@ -1,7 +1,11 @@
 package leo.work.support.biz;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,38 +24,41 @@ import leo.work.support.base.biz.CommonLifeBiz;
  * 代码备注:
  * ---------------------------------------------------------------------------------------------
  **/
-public class SmsCountdownBiz extends CommonLifeBiz {
+public class SmsCountdownBiz extends CommonLifeBiz implements Handler.Callback {
 
     //总时长
-    private int totalDuration;
+    private final int totalDuration;
     //当前时长
     private int currentDuration;
     //倒计时
     private Timer timer;
     //倒计时回调
-    private OnSmsCountdownBizCallBack callBack;
+    private final OnSmsCountdownBizCallBack callBack;
     //点击发送短信的按钮
-    private View view;
+    private final View view;
+    //
+    private Handler handler;
 
     public SmsCountdownBiz(LifeControlInterface lifeControlInterface, View view, int totalDuration, OnSmsCountdownBizCallBack callBack) {
         super(lifeControlInterface);
         this.view = view;
         this.totalDuration = totalDuration;
         this.callBack = callBack;
-        this.callBack.onSmsPrepare();
+        sendMessageToHandler(2, null);
     }
 
     //请求服务器
     public void requestService() {
-        view.setClickable(false);
-        callBack.onSmsPrepare();
+        sendMessageToHandler(3, false);
+        sendMessageToHandler(2, null);
     }
 
     //请求服务器得到的结果
     public void serviceResult(boolean success) {
+        currentDuration = 0;
         if (!success) {
-            view.setClickable(true);
-            callBack.onSmsPrepare();
+            sendMessageToHandler(3, true);
+            sendMessageToHandler(2, null);
             return;
         }
         startCountdown();
@@ -59,35 +66,71 @@ public class SmsCountdownBiz extends CommonLifeBiz {
 
     //开始倒计时
     private void startCountdown() {
-        if (timer == null) {
-            timer = new Timer();
+        if (timer != null) {
+            timer.cancel();
         }
-        timer.cancel();
+        timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 int seconds = totalDuration - currentDuration;
+                //倒计时结束
                 if (seconds <= 0) {
                     countdownEnd();
-                } else {
-                    callBack.onSmsTimeChange(seconds);
-                    currentDuration++;
+                    return;
                 }
+                //
+                sendMessageToHandler(1, seconds);
+                //
+                currentDuration++;
             }
-        }, 1000);
+        }, 0, 1000);
     }
 
     //倒计时结束
     private void countdownEnd() {
-        if (callBack != null) {
-            callBack.onSmsPrepare();
-        }
-        if (view != null) {
-            view.setClickable(true);
-        }
+        sendMessageToHandler(2, null);
+        sendMessageToHandler(3, true);
         if (timer != null) {
             timer.cancel();
         }
+        timer = null;
+        currentDuration = 0;
+    }
+
+    //
+    private void sendMessageToHandler(int what, Object obj) {
+        if (handler == null) {
+            handler = new Handler(this);
+        }
+        Message message = obj != null ? handler.obtainMessage(what, obj) : handler.obtainMessage(what);
+        handler.sendMessage(message);
+    }
+
+    //
+    @Override
+    public boolean handleMessage(@NonNull Message message) {
+        switch (message.what) {
+            case 1: {
+                if (callBack != null) {
+                    callBack.onSmsTimeChange((Integer) message.obj);
+                }
+                break;
+            }
+            case 2: {
+                if (callBack != null) {
+                    callBack.onSmsPrepare();
+                }
+                break;
+            }
+            case 3: {
+                if (view != null) {
+                    view.setClickable((Boolean) message.obj);
+                }
+                break;
+            }
+        }
+        return false;
     }
 
     public interface OnSmsCountdownBizCallBack {
@@ -107,5 +150,10 @@ public class SmsCountdownBiz extends CommonLifeBiz {
             timer.cancel();
         }
         timer = null;
+
+        if (handler != null) {
+            handler.removeCallbacksAndMessages(null);
+        }
+        handler = null;
     }
 }
