a09367f37233f18ff9457e8752592a7cecf233c6
liuguian <LiuGuian1313>
2022-07-11 17:28:02 +0800

    修改名字

--- leosupport/src/main/java/leo/work/support/base/activity/CommonAbstractActivity.java
+++ leosupport/src/main/java/leo/work/support/base/activity/CommonAbstractActivity.java
@@ -14,94 +14,93 @@ import java.util.List;
 
 import leo.work.support.base.LifeControlInterface;
 import leo.work.support.base.biz.CommonLifeBiz;
-import leo.work.support.util.CommonUtil;
 
 
 public abstract class CommonAbstractActivity extends AppCompatActivity implements LifeControlInterface {
 
-    private List<CommonLifeBiz> lifeBizList;
+    private List<CommonLifeBiz> bizList;
 
     @Override
     public void addLifeCallBackList(CommonLifeBiz biz) {
-        if (lifeBizList == null) {
-            lifeBizList = new ArrayList<>();
+        if (bizList == null) {
+            bizList = new ArrayList<>();
         }
         if (biz == null) {
             return;
         }
-        lifeBizList.add(biz);
+        bizList.add(biz);
     }
 
     @Override
     public void removeLifeCallBackList(CommonLifeBiz biz) {
-        if (lifeBizList == null) {
+        if (bizList == null) {
             return;
         }
         if (biz == null) {
             return;
         }
-        Log.e("liu0708", "Activity    准备删除生命周期对象    lifeBizList = " + lifeBizList.size());
-        lifeBizList.remove(biz);
-        Log.e("liu0708", "Activity    删除完成生命周期对象    lifeBizList = " + lifeBizList.size());
+        Log.e("liu0708", "Activity    准备删除生命周期对象    lifeBizList = " + bizList.size());
+        bizList.remove(biz);
+        Log.e("liu0708", "Activity    删除完成生命周期对象    lifeBizList = " + bizList.size());
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        for (int i = 0; lifeBizList != null && i < lifeBizList.size(); i++) {
-            lifeBizList.get(i).onStart();
+        for (int i = 0; bizList != null && i < bizList.size(); i++) {
+            bizList.get(i).onStart();
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        for (int i = 0; lifeBizList != null && i < lifeBizList.size(); i++) {
-            lifeBizList.get(i).onResume();
+        for (int i = 0; bizList != null && i < bizList.size(); i++) {
+            bizList.get(i).onResume();
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        for (int i = 0; lifeBizList != null && i < lifeBizList.size(); i++) {
-            lifeBizList.get(i).onPause();
+        for (int i = 0; bizList != null && i < bizList.size(); i++) {
+            bizList.get(i).onPause();
         }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        for (int i = 0; lifeBizList != null && i < lifeBizList.size(); i++) {
-            lifeBizList.get(i).onStop();
+        for (int i = 0; bizList != null && i < bizList.size(); i++) {
+            bizList.get(i).onStop();
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        for (int i = 0; lifeBizList != null && i < lifeBizList.size(); i++) {
-            lifeBizList.get(i).onDestroy();
+        for (int i = 0; bizList != null && i < bizList.size(); i++) {
+            bizList.get(i).onDestroy();
         }
-        if (lifeBizList != null) {
-            lifeBizList.clear();
+        if (bizList != null) {
+            bizList.clear();
         }
-        lifeBizList = null;
+        bizList = null;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        for (int i = 0; lifeBizList != null && i < lifeBizList.size(); i++) {
-            lifeBizList.get(i).onActivityResult(requestCode, resultCode, data);
+        for (int i = 0; bizList != null && i < bizList.size(); i++) {
+            bizList.get(i).onActivityResult(requestCode, resultCode, data);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        for (int i = 0; lifeBizList != null && i < lifeBizList.size(); i++) {
-            lifeBizList.get(i).onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int i = 0; bizList != null && i < bizList.size(); i++) {
+            bizList.get(i).onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
@@ -109,23 +108,23 @@ public abstract class CommonAbstractActivity extends AppCompatActivity implement
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        for (int i = 0; lifeBizList != null && i < lifeBizList.size(); i++) {
-            lifeBizList.get(i).onSaveInstanceState(outState);
+        for (int i = 0; bizList != null && i < bizList.size(); i++) {
+            bizList.get(i).onSaveInstanceState(outState);
         }
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        for (int i = 0; lifeBizList != null && i < lifeBizList.size(); i++) {
-            lifeBizList.get(i).onRestoreInstanceState(savedInstanceState);
+        for (int i = 0; bizList != null && i < bizList.size(); i++) {
+            bizList.get(i).onRestoreInstanceState(savedInstanceState);
         }
     }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
-        for (int i = 0; lifeBizList != null && i < lifeBizList.size(); i++) {
-            lifeBizList.get(i).dispatchTouchEvent(event);
+        for (int i = 0; bizList != null && i < bizList.size(); i++) {
+            bizList.get(i).dispatchTouchEvent(event);
         }
         return super.dispatchTouchEvent(event);
     }
