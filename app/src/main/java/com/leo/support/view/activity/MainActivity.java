115f5aeee27849c0e62e5ee5dd3b4e5bb2c515f2
LeoLau <742831652@qq.com>
2022-10-10 23:41:08 +0800

    修改

--- app/src/main/java/com/leo/support/view/activity/MainActivity.java
+++ app/src/main/java/com/leo/support/view/activity/MainActivity.java
@@ -1,45 +1,52 @@
 package com.leo.support.view.activity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.leo.support.R;
+import com.leo.support.databinding.ActivityMainBinding;
 
+import leo.work.support.base.activity.CommonActivity;
+import leo.work.support.biz.PermissionBiz;
 import leo.work.support.biz.WorkFlow.CommonWorkFlowBiz;
 import leo.work.support.biz.WorkFlow.WorkFlowControl;
 import leo.work.support.biz.WorkFlow.WorkFlowTask;
 import leo.work.support.support.toolSupport.LeoSupport;
 
-public class MainActivity extends Activity {
+public class MainActivity extends CommonActivity<ActivityMainBinding> {
+
+    private PermissionBiz permissionBiz;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        LeoSupport.fullScreen(this, false);
-        setContentView(R.layout.activity_main);
+    protected int setLayout() {
+        return R.layout.activity_main;
+    }
 
-        new CommonWorkFlowBiz(control -> {
-            control.doNextTask();
-        }).addWorkFlowTask(new WorkFlowTask(1) {
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+        LeoSupport.fullScreen(this, false);
+        permissionBiz = new PermissionBiz(this);
+        String[] permissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        permissionBiz.checkPermission(permissions, 10002, new PermissionBiz.OnPermissionBizCallBack() {
             @Override
-            public void doTask(WorkFlowTask lastWorkFlowTask, WorkFlowControl control) {
-                control.doTaskById(3);
+            public void onPermissionSuccess(int requestCode, String[] permissions) {
             }
-        }).addWorkFlowTask(new WorkFlowTask(2) {
+
             @Override
-            public void doTask(WorkFlowTask lastWorkFlowTask, WorkFlowControl control) {
-                control.doNextTask();
+            public void onPermissionFail(int requestCode, String[] successPermissions, String[] failPermissions) {
+
             }
-        }).addWorkFlowTask(new WorkFlowTask(3) {
-            @Override
-            public void doTask(WorkFlowTask lastWorkFlowTask, WorkFlowControl control) {
-                control.doNextTask();
-            }
-        }).startTask(() -> {
         });
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+
     }
 
 }
